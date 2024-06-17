fc8ce9706497c52615cca47715fe483e8ee3208d
Grzegorz Muczyński <s27971@pjwstk.edu.pl>
2024-06-17 09:30:49 +0200

    com

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -13,9 +13,7 @@ public static class SeedData
 {
     public static void Initialize(IServiceProvider serviceProvider)
     {
-        using (var context = new MvcMovieContext(
-            serviceProvider.GetRequiredService<
-                DbContextOptions<MvcMovieContext>>()))
+        using (var context = new MvcMovieContext(serviceProvider.GetRequiredService<DbContextOptions<MvcMovieContext>>()))
         {
             // Look for any movies.
             if (context.Movie.Any())
